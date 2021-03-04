0b143235b66fbaaebdd801260b7fc55363b741b1
Martin <matovu.synergy@gmail.com>
2021-03-04 06:14:07 +0300

    Remove sidebar from single-product page (woocommerce)

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -7,3 +7,17 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Remove sidebar from single-product page.
+ */
+add_filter( 'generate_sidebar_layout', 'gpc_woo_sidebar' );
+function gpc_woo_sidebar( $layout ) {
+ 	// If we are on a woocommerce page, remove the sidebar
+    if ( function_exists( 'is_woocommerce' ) && is_woocommerce() ) {
+        return 'no-sidebar';
+    }
+
+    // Or else, set the regular layout
+    return $layout;
+}

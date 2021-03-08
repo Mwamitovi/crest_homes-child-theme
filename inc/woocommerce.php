c4ea63b17d1669b34cb4261714a7c7de4ef3f6b7
Martin <matovu.synergy@gmail.com>
2021-03-08 13:28:51 +0300

    Remove 'categories' from single-product description

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -29,3 +29,8 @@ add_action( 'init', 'gpc_remove_wc_breadcrumbs' );
 function gpc_remove_wc_breadcrumbs() {
     remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
 }
+
+/* 
+ * Remove Categories from Single Products (description)
+ */ 
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
